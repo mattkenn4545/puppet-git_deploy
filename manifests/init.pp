cb87b3a7323aa9b175e476affb0cb48ac608fbbf
Matthew Kennedy <mattkenn4545@gmail.com>
2014-05-09 23:34:59 -0600

    MVP

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class git_deploy (
-  $project_path = '/opt/projects'
+  $project_path     = '/opt/projects'
 ) {
   include stdlib
 
@@ -12,25 +12,33 @@ class git_deploy (
   } ->
 
   file { '/home/git/.ssh':
-    ensure  => directory,
-    mode    => '0750',
-    owner   => 'git',
-    group   => 'git',
-    require => User[ 'git' ]
+    ensure      => directory,
+    mode        => '0750',
+    owner       => 'git',
+    group       => 'git',
+    require     => User[ 'git' ]
   } ->
 
   ssh_authorized_key { 'matt':
-    ensure    => present,
-    type      => 'ssh-rsa',
-    key       => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCzTy59LkeFxmZHnKVo0sJ3cfhFsOYCzqqr03uVjY77lhLPUPVjT6XImVBVvvJ+OX5J6yzk1YIObLVCcdEx+aSmL5rsQSrXferWuOKiv4bfb8rpMjUK04MIhBj8ZKI61Dah3VceUO33Q2VcH8XAhmQb0kUstd3GYcloh+pWEWVEo2WmT3+e9hI5/f4LpC6VkiEY1Rt0y51mFiljj62sedkFsjNAdNE6mWly/k03fEVtSRc74W/Y7clt74Z6LIYlTZnp9fhi9p7iI5zYHv9xoL5m1d2bRCJSXtBfFmuR2cdf1B5kcPJEnA5zpNLKQpY0lLhruJye9Slh5etn2v3wZdJP',
-    user      => 'git',
+    ensure      => present,
+    type        => 'ssh-rsa',
+    key         => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCzTy59LkeFxmZHnKVo0sJ3cfhFsOYCzqqr03uVjY77lhLPUPVjT6XImVBVvvJ+OX5J6yzk1YIObLVCcdEx+aSmL5rsQSrXferWuOKiv4bfb8rpMjUK04MIhBj8ZKI61Dah3VceUO33Q2VcH8XAhmQb0kUstd3GYcloh+pWEWVEo2WmT3+e9hI5/f4LpC6VkiEY1Rt0y51mFiljj62sedkFsjNAdNE6mWly/k03fEVtSRc74W/Y7clt74Z6LIYlTZnp9fhi9p7iI5zYHv9xoL5m1d2bRCJSXtBfFmuR2cdf1B5kcPJEnA5zpNLKQpY0lLhruJye9Slh5etn2v3wZdJP',
+    user        => 'git'
   }
 
   file { $project_path:
-    ensure  => directory,
-    mode    => '0750',
-    owner   => 'git',
-    group   => 'git',
-    require => User[ 'git' ],
+    ensure      => directory,
+    mode        => '0750',
+    owner       => 'git',
+    group       => 'git',
+    require     => User[ 'git' ]
+  }
+
+  file { '/var/log/git_deploy':
+    ensure      => directory,
+    mode        => '0755',
+    owner       => 'git',
+    group       => 'git',
+    require     => User[ 'git' ]
   }
 }
--- manifests/project.pp
+++ manifests/project.pp
@@ -1,21 +1,55 @@
-define git_deploy::project {
+define git_deploy::project (
+  $destination,
+  $git_user_groups = []
+) {
   include git_deploy
 
-  $project_path = $git_deploy::project_path
+  $project_path       = $git_deploy::project_path
+  $full_project_path  = "${project_path}/${name}.git"
 
-  file { "${project_path}/${name}.git":
+  File <| title == $destination |> {
+    mode        => '0770'
+  }
+
+  User <| title == 'git' |> {
+    groups      => $git_user_groups
+  }
+
+  User[ 'git' ] ->
+  exec { "${name} git init":
+    cwd         => $destination,
+    creates     => "${destination}/.git",
+    user        => 'git',
+    command     => 'git init',
+    require     => Package[ 'git' ]
+  } ~>
+  exec { "${name} git remote add":
+    cwd         => $destination,
+    user        => 'git',
+    command     => "git remote add origin ${full_project_path}",
+    refreshonly => true
+  }
+
+  User[ 'git' ] ->
+  file { $full_project_path:
     ensure => directory,
-    mode    => '0750',
-    owner   => 'git',
-    group   => 'git',
-    require => [ User[ 'git' ], File[$project_path] ],
+    mode        => '0750',
+    owner       => 'git',
+    group       => 'git',
+    require     => File[ $project_path ]
   } ->
-
   exec { "${name} git --bare init":
-    cwd     => "${project_path}/${name}.git",
-    creates => "${project_path}/${name}.git/description",
-    user    => 'git',
-    command => 'git --bare init',
-    require => Package[ 'git' ]
+    cwd         => $full_project_path,
+    creates     => "${full_project_path}/description",
+    user        => 'git',
+    command     => 'git --bare init',
+    require     => Package[ 'git' ]
+  } ->
+  file { "${full_project_path}/hooks/post-receive":
+    ensure      => present,
+    mode        => '0750',
+    owner       => 'git',
+    group       => 'git',
+    content     => template("${module_name}/post-receive")
   }
 }
