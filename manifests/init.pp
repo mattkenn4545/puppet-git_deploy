c86eef71c086a1a0f1abbd90b3a42e56c27f87e2
Matthew Kennedy <mattkenn4545@gmail.com>
2014-05-11 17:32:26 -0600

    Pass in the deploy key

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,6 @@
 class git_deploy (
-  $project_path     = '/opt/projects'
+  $key            = false,
+  $project_path   = '/opt/projects'
 ) {
   include stdlib
 
@@ -17,13 +18,16 @@ class git_deploy (
     owner       => 'git',
     group       => 'git',
     require     => User[ 'git' ]
-  } ->
+  }
 
-  ssh_authorized_key { 'matt':
-    ensure      => present,
-    type        => 'ssh-rsa',
-    key         => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCzTy59LkeFxmZHnKVo0sJ3cfhFsOYCzqqr03uVjY77lhLPUPVjT6XImVBVvvJ+OX5J6yzk1YIObLVCcdEx+aSmL5rsQSrXferWuOKiv4bfb8rpMjUK04MIhBj8ZKI61Dah3VceUO33Q2VcH8XAhmQb0kUstd3GYcloh+pWEWVEo2WmT3+e9hI5/f4LpC6VkiEY1Rt0y51mFiljj62sedkFsjNAdNE6mWly/k03fEVtSRc74W/Y7clt74Z6LIYlTZnp9fhi9p7iI5zYHv9xoL5m1d2bRCJSXtBfFmuR2cdf1B5kcPJEnA5zpNLKQpY0lLhruJye9Slh5etn2v3wZdJP',
-    user        => 'git'
+  if $key {
+    ssh_authorized_key { 'deploy':
+      ensure      => present,
+      type        => 'ssh-rsa',
+      key         => $key,
+      user        => 'git',
+      require     => File[ '/home/git/.ssh' ]
+    }
   }
 
   file { $project_path:
